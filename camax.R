2acaa7814d3bdd8a7c2b70d42234df85e132b706
yvemeng <meng.yve@gmail.com>
2014-05-25 14:10:39 +0800

    add

---
+++ camax.R
@@ -0,0 +1,42 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+       m <- NULL
+       set <- function(y) {
+              x <<- y
+              m <<- NULL
+       }
+       get <- function() x
+       setsolve <- function(solve) m <<- solve
+       getsolve <- function() m
+       list(set = set, get = get,
+            setsolve  = setsolve ,
+            getsolve  = getsolve )       
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+       m <- x$getsolve()
+       if(!is.null(m)) {
+              message("getting cached data")
+              return(m)
+       }
+       data <- x$get()
+       m <- solve(data, ...)
+       x$setsolve(m)
+       m         
+        ## Return a matrix that is the inverse of 'x'
+}
+
+
+aa <-  matrix(c(4,2,7,6) , 2,2)
+makeCacheMatrix(aa)
+cacheSolve(makeCacheMatrix(aa) )
+
+
+
---
+++ readme.txt
@@ -0,0 +1,4 @@
+
+This is a function about making an inverse matrix of a matrix assigned. 
+
+two main functions are included to complete the job.
